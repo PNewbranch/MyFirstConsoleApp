046cbd68ce2cd02eb4ff77423bbda988103d5bee
P N <p.newbranch@outlook.com>
2019-10-21 12:24:37 +0200

    första test

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Hallå");
+            Console.Write("Hej");
+        }
+    }
+}
